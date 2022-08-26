681e8281a038b72e8ecc964d64731cb1b08335ff
Dan Rees <danrees8743@gmail.com>
2022-08-26 19:38:41 +0100

    Init commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+// Selcting elements
+
+// Starting conditions
+
+// Initalizing the game
+
+// Rolling dice functionality
+
+// Generate a random dice roll
+
+// Display Dice image
+
+// Check for rolled 1: if true,
+
+// Add dice to current score
+
+// switch no next player
+
+// Add current score to active player's score
+
+// Check if player's score is > 100
+
+// Finish game
